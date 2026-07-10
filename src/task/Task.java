81df6dcdaa5c684ae54dbfb35037aed3e56214f8
Jackie Fang <jfang13@depaul.edu>
2026-07-10 14:56:02 -0500

    Create Task class

---
+++ src/task/Task.java
@@ -0,0 +1,45 @@
+package task;
+
+public class Task {
+
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markCompleted() {
+        completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title +
+               "\nDescription: " + description +
+               "\nCompleted: " + completed;
+    }
+}
